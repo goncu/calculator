c2f0f2b67f631623ec24197c912b90c476f5d4b0
goncu <116492383+goncu@users.noreply.github.com>
2023-02-22 11:30:01 +0200

    Complete design

---
+++ script.js
@@ -0,0 +1,3 @@
+// use while loop for before operator
+//use a variable for input and assign it to all button inputs, then do a while loop with the condition that it is not an operator
+//use a counter for calculating tens, hundreds etc.
